d0040daf3a8afc54434596648cb8cb1b91721635
Anita Kemp and Susan Kralian <anita@>
2019-08-28 15:05:26 -0700

    write code to switch players in the console log

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,55 @@
 // Front End//
 $(document).ready(function(){
-  $("#rollDice").click(function(event) {
-    event.preventDefault();
-    $(".container p ").text(roll());
+  $("#rollDice").click(function() {
 
-    var test = roll();
-    console.log(myplayer.addScore(test));
+    // $(".container p ").text(myplayer.addScore());
+    function roll() {
+      return Math.floor(Math.random() *6) + 1
+    }
 
+    var numberBuffer = roll()
+    var player1 = new Player("player1")
+    var player2 = new Player("player2")
+    var players = [player1, player2];
+    var turn = Math.round(Math.random());
+    var currentPlayer = players[turn];
+
+    function switchPlayers (players, currentPlayer){
+      if(currentPlayer === players[0]) {
+        currentPlayer = players[1];
+      $("#player2").addClass(".playerturn")
+      $("#player1").removeClass(".playerturn")
+      console.log("We're here")
+    } else {
+      currentPlayer = players[0];
+      $("#player1").addClass(".playerturn")
+      $("#player2").removeClass(".playerturn")
+    }
+    console.log(currentPlayer);
+  }
+
+      if (numberBuffer === 1) {
+        console.log ("LOSE");
+        switchPlayers(players, currentPlayer);
+      } else {
+        $(".container p ").text(myplayer.addScore(roll()));
+      }
+    });
   });
-});
-
 
 // document.getElementById("rollDice").addEventListener("click", roll);
 //
 
 
 //Back End//
-
-
-
+function Player (player, score){
+  this.player = player,
+  this.index = [0]
+}
 
 function Turn (numberRolled, score) {
-  this.numberRolled = numberRolled
-  this.score = score
- }
-var myplayer = new Turn (1, 0)
-
-
-
- function roll() {
-   return Math.floor(Math.random() *6) + 1
+  this.numberRolled = numberRolled,
+  this.score = 0
  }
 
  Turn.prototype.addScore = function(numberRolled){
@@ -37,18 +57,5 @@ var myplayer = new Turn (1, 0)
    return this.score;
  }
 
-// Turn.prototype.roll  = function() {
-//     roll()
-//     if (this.numberRolled === 1) {
-//      return false;
-//    }else{
-//      this.score += numberRolled;
-//      console.log("we got here");
-//    }
-
-
-// }
-
-// Turn.prototype.addScore = function(){
-//   this.score
-// }
+ var myplayer = new Turn (0, 0)
+ var myopponent = new Turn (0, 0)
