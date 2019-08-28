631c6773d9e92e906789ccd049220de09d0ef487
Anita Kemp and Susan Kralian <anita@>
2019-08-28 11:10:27 -0700

    Add addScore prototype function

--- js/scripts.js
+++ js/scripts.js
@@ -2,21 +2,53 @@
 $(document).ready(function(){
   $("#rollDice").click(function(event) {
     event.preventDefault();
-    roll();
+    $(".container p ").text(roll());
+
+    var test = roll();
+    console.log(myplayer.addScore(test));
+
   });
 });
 
 
-document.getElementById("rollDice").addEventListener("click", roll);
-
+// document.getElementById("rollDice").addEventListener("click", roll);
+//
 
 
 //Back End//
-// var myplayer = new Turn (numberRolled, score)
 
-// function Turn (numberRolled, score) {
-//   this.numberRolled = numberRolled,
-//   this.score = score
+
+
+
+function Turn (numberRolled, score) {
+  this.numberRolled = numberRolled
+  this.score = score
+ }
+var myplayer = new Turn (1, 0)
+
+
+
+ function roll() {
+   return Math.floor(Math.random() *6) + 1
+ }
+
+ Turn.prototype.addScore = function(numberRolled){
+   this.score += numberRolled;
+   return this.score;
+ }
+
+// Turn.prototype.roll  = function() {
+//     roll()
+//     if (this.numberRolled === 1) {
+//      return false;
+//    }else{
+//      this.score += numberRolled;
+//      console.log("we got here");
+//    }
+
+
 // }
 
-function roll() {console.log (Math.floor(Math.random() *6) + 1)}
+// Turn.prototype.addScore = function(){
+//   this.score
+// }
