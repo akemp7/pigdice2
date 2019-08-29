a0368f700f44c89249e6e114085b4ff88c89e8c8
Anita Kemp and Susan Kralian <anita@>
2019-08-29 14:11:14 -0700

    fix introduced bug so that player one correctly adds for each roll

--- js/scripts.js
+++ js/scripts.js
@@ -1,54 +1,59 @@
+var player1 = new Player("player1", 0)
+var player2 = new Player("player2", 1)
+
+function roll() {
+  return Math.ceil(Math.random() * 6)
+}
+
 // Front End//
-$(document).ready(function(){
+$(document).ready(function() {
   // $("#start").click(function() {
   //   var players = [myplayer, myopponent];
   // });
-    $("#rollDice").click(function() {
-      function roll () {
-      return Math.floor(Math.random() *6) + 1
+  $("#rollDice").click(function() {
+
+    var numberBuffer = roll();
+    var tempScore = 0;
+    var players = [player1, player2];
+
+    // function switchPlayers (players, currentPlayer){
+    //     if(numberBuffer===1) {
+    //       (currentPlayer === players[0])
+    //       currentPlayer = players[1];
+    //     $("#player1").addClass("playerturn")
+    //     $("#player2").removeClass("playerturn")
+    //     console.log(currentPlayer)
+    //   } else {
+    //     currentPlayer = players[0];
+    //     $("#player2").addClass("playerturn")
+    //     $("#player1").removeClass("playerturn")
+    //   }
+    // }
+    // $("#player1").append(player1.addScore(roll()));
+    // $("#player1").empty().append(player1.addScore(roll()));
+    if (numberBuffer !== 1) {
+      console.log(numberBuffer);
+      $("#player1").text(player1.addScore(numberBuffer));
+      // $("#player2").append(player2.addScore(roll(tempScore)));
+      // $("#player2").empty().append(player1.addScore(roll(tempScore)));
+
+    } else if (numberBuffer === 1) {
+
+      // return currentPlayer = switchPlayers(currentPlayer, players);
+      var players = [player2, player1];
+      $("#player2").text(player2.addScore(numberBuffer));
     }
-      var numberBuffer = roll();
-
-      var player1 = new Player ("player1")
-      var player2 = new Player ("player2")
-      var players = [player1, player2];
-      // var turn = Math.round(Math.random());
-      // var currentPlayer = [0];
-      var tempScore = 0;
-
-      // function switchPlayers (players, currentPlayer){
-      //     if(numberBuffer===1) {
-      //       (currentPlayer === players[0])
-      //       currentPlayer = players[1];
-      //     $("#player1").addClass("playerturn")
-      //     $("#player2").removeClass("playerturn")
-      //     console.log(currentPlayer)
-      //   } else {
-      //     currentPlayer = players[0];
-      //     $("#player2").addClass("playerturn")
-      //     $("#player1").removeClass("playerturn")
-      //   }
-      // }
-      // $("#player1").append(player1.addScore(roll()));
-      // $("#player1").empty().append(player1.addScore(roll()));
-      if (numberBuffer!==1) {
-        $("#player1").append(player2.addScore(roll()));
-        $("#player1").empty().append(player2.addScore(roll()));
-      } else if (numberBuffer===1) {
-
-        // return currentPlayer = switchPlayers(currentPlayer, players);
-        var players = [player2, player1];
-        $("#player2").append(player2.addScore(roll()));
-        $("#player2").empty().append(player2.addScore(roll()));
-    // $(".container p ").text(myplayer.addScore());
+    // }else if (numberBuffer===1) {
+    //   var players = [player1, player2];
+    //     $("#player1").append(player1.addScore(roll()));
+    //     $("#player1").empty().append(player1.addScore(roll()));}
 
 
-     // $("#player2").append(myplayer.addScore(roll()));
-     // $("#player2").empty().append(myplayer.addScore(roll()));
-   // };
+    // $("#player2").append(myplayer.addScore(roll()));
+    // $("#player2").empty().append(myplayer.addScore(roll()));
+    // };
 
-};
-});
+  });
 });
 // if (numberBuffer === 1){
 //   currentPlayer = switchPlayers ();
@@ -56,36 +61,31 @@ $(document).ready(function(){
 //   $("#player2").empty().append(myplayer.addScore(roll()));
 // }
 
-
-
 // document.getElementById("rollDice").addEventListener("click", roll);
 //
 
 //Back End//
 
-
-function Turn (numberRolled, score, index) {
+function Turn(numberRolled, score, index) {
   this.numberRolled = numberRolled,
-  this.score = 0,
-  this.index = index
+    this.score = 0,
+    this.index = index
 
 }
 
+//
+//  Turn.prototype.addScore = function(tempScore){
+//    this.score += tempScore;
+//    return this.score;
+// }
 
- Turn.prototype.addScore = function(numberRolled){
-   this.score += numberRolled;
-   return this.score;
-}
-
-Player.prototype.addScore = function(tempScore){
+Player.prototype.addScore = function(tempScore) {
   this.score += tempScore;
   return this.score;
 }
 //  //
 
-function Player (name, score) {
+function Player(name, score) {
   this.name = name,
-  this.score = 0
+    this.score = 0
 }
- // var myplayer = new Turn (0, 0, 0)
- // var myopponent = new Turn (0, 0,1)
