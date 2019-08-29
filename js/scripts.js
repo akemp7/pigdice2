87d5032975d00f9db74bc1fb7e88bc9df34f5b48
Anita Kemp and Susan Kralian <anita@>
2019-08-29 11:11:06 -0700

    write code to switch based on value of role

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,61 @@
 // Front End//
 $(document).ready(function(){
-  $("#rollDice").click(function() {
-
-    // $(".container p ").text(myplayer.addScore());
-    function roll() {
+  // $("#start").click(function() {
+  //   var players = [myplayer, myopponent];
+  // });
+    $("#rollDice").click(function() {
+      function roll () {
       return Math.floor(Math.random() *6) + 1
     }
+      var numberBuffer = roll();
 
-    var numberBuffer = roll();
-    var players = [player1, player2];
-    var turn = Math.round(Math.random());
-    var currentPlayer = players[turn];
+      var player1 = new Player ("player1")
+      var player2 = new Player ("player2")
+      var players = [player1, player2];
+      // var turn = Math.round(Math.random());
+      // var currentPlayer = [0];
+      var tempScore = 0;
 
-    function switchPlayers (players, currentPlayer){
-      if(currentPlayer === players[0]) {
-        currentPlayer = players[1];
-      $("#player2").addClass("playerturn")
-      $("#player1").removeClass("playerturn")
-      console.log(currentPlayer)
-    } else {
-      currentPlayer = players[0];
-      $("#player1").addClass("playerturn")
-      $("#player2").removeClass("playerturn")
-    }
-  }
+      // function switchPlayers (players, currentPlayer){
+      //     if(numberBuffer===1) {
+      //       (currentPlayer === players[0])
+      //       currentPlayer = players[1];
+      //     $("#player1").addClass("playerturn")
+      //     $("#player2").removeClass("playerturn")
+      //     console.log(currentPlayer)
+      //   } else {
+      //     currentPlayer = players[0];
+      //     $("#player2").addClass("playerturn")
+      //     $("#player1").removeClass("playerturn")
+      //   }
+      // }
+      // $("#player1").append(player1.addScore(roll()));
+      // $("#player1").empty().append(player1.addScore(roll()));
+      if (numberBuffer!==1) {
+        $("#player1").append(player2.addScore(roll()));
+        $("#player1").empty().append(player2.addScore(roll()));
+      } else if (numberBuffer===1) {
 
-      if (numberBuffer === 1) {
-        console.log ("LOSE");
-        switchPlayers(players, currentPlayer);
-      } else {
-        $("#player1").append(myplayer.addScore(roll()));
-        $("#player1").empty().append(myplayer.addScore(roll()));
-        $("#player2").append(myopponent.addScore(roll()));
-        $("#player2").empty().append(myplayer.addScore(roll()));
-      }
-    });
-  });
-
-Turn.prototype.startGame = function (){
-  myplayer =players [0],
-
-
-
-  };
+        // return currentPlayer = switchPlayers(currentPlayer, players);
+        var players = [player2, player1];
+        $("#player2").append(player2.addScore(roll()));
+        $("#player2").empty().append(player2.addScore(roll()));
+    // $(".container p ").text(myplayer.addScore());
 
 
+     // $("#player2").append(myplayer.addScore(roll()));
+     // $("#player2").empty().append(myplayer.addScore(roll()));
+   // };
 
+};
+});
+});
+// if (numberBuffer === 1){
+//   currentPlayer = switchPlayers ();
+//   $("#player2").append(myplayer.addScore(roll()));
+//   $("#player2").empty().append(myplayer.addScore(roll()));
+// }
 
-}
 
 
 // document.getElementById("rollDice").addEventListener("click", roll);
@@ -55,10 +63,11 @@ Turn.prototype.startGame = function (){
 
 //Back End//
 
+
 function Turn (numberRolled, score, index) {
   this.numberRolled = numberRolled,
   this.score = 0,
-  this.index = 0
+  this.index = index
 
 }
 
@@ -66,15 +75,17 @@ function Turn (numberRolled, score, index) {
  Turn.prototype.addScore = function(numberRolled){
    this.score += numberRolled;
    return this.score;
+}
 
-
- //(Click button to start game or pick color or X)
-   Turn.prototype.addIndex = function () {
-       this.Turn.push(index);
-       Turn.index+=  this.index;
-       return this;
-   };
-
+Player.prototype.addScore = function(tempScore){
+  this.score += tempScore;
+  return this.score;
+}
 //  //
- var myplayer = new Turn (0, 0)
- var myopponent = new Turn (0, 0)
+
+function Player (name, score) {
+  this.name = name,
+  this.score = 0
+}
+ // var myplayer = new Turn (0, 0, 0)
+ // var myopponent = new Turn (0, 0,1)
